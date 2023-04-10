f50cf417e8e146f5ca00090679dbdc51a648d428
victorbillaud <victorbillaud@gmail.com>
2023-04-10 12:09:28 +0200

    update migration

--- supabase/migrations/20230410100130_spot_slug.sql
+++ supabase/migrations/20230410100130_spot_slug.sql
@@ -1,3 +1,6 @@
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
+
+
 create extension if not exists "unaccent" with schema "public" version '1.1';
 
 drop view if exists "public"."spot_extanded_view";
@@ -160,6 +163,54 @@ using (true);
 
 CREATE TRIGGER spots_slug_generator BEFORE INSERT OR UPDATE ON public.spots FOR EACH ROW EXECUTE FUNCTION generate_spot_slug();
 
+
+create policy "Anyone can upload an avatar."
+on "storage"."objects"
+as permissive
+for insert
+to public
+with check ((bucket_id = 'avatars'::text));
+
+
+create policy "Avatar images are publicly accessible."
+on "storage"."objects"
+as permissive
+for select
+to public
+using ((bucket_id = 'avatars'::text));
+
+
+create policy "Insert 1ffg0oo_0"
+on "storage"."objects"
+as permissive
+for insert
+to authenticated
+with check ((bucket_id = 'images'::text));
+
+
+create policy "Insert 1ffg0oo_1"
+on "storage"."objects"
+as permissive
+for update
+to authenticated
+using ((bucket_id = 'images'::text));
+
+
+create policy "Insert 1ffg0oo_2"
+on "storage"."objects"
+as permissive
+for delete
+to authenticated
+using ((bucket_id = 'images'::text));
+
+
+create policy "Select 1ffg0oo_3"
+on "storage"."objects"
+as permissive
+for select
+to anon
+using ((bucket_id = 'images'::text));
+
 -- Execute script to fill slug column
 WITH spot_locations AS (
     SELECT
@@ -185,6 +236,3 @@ FROM
     spot_locations
 WHERE
     public.spots.id = spot_locations.id;
-
-
-
