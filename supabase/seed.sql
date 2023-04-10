e7f44f9fec441f2a191a4c0f1b887230ecfdd677
victorbillaud <victorbillaud@gmail.com>
2023-04-10 12:45:46 +0200

    seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,15 @@
+create or replace function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.profiles (id, full_name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
 -- Set up Storage!
 insert into storage.buckets (id, name)
   values ('avatars', 'avatars');
