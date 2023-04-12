8b84613745a7ef453b864764be33375397817b40
victorbillaud <victorbillaud@gmail.com>
2023-04-12 22:52:15 +0200

    Add next themes to manage theming

--- src/components/ColorSchemeProvider.tsx
+++ src/components/ColorSchemeProvider.tsx
@@ -1,31 +1,14 @@
 'use client';
 
-import usePreferredColorScheme, { ColorScheme } from '@/hooks/useColorScheme';
-import { logger } from '@/lib/logger';
-import React, { createContext, useContext, useEffect } from 'react';
-
-const ColorSchemeContext = createContext<{
-  colorScheme: ColorScheme;
-}>({
-  colorScheme: 'light',
-});
-
-export const useColorScheme = () => {
-  return useContext(ColorSchemeContext);
-};
+import { ThemeProvider } from 'next-themes';
+import React from 'react';
 
 export const ColorSchemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const colorScheme = usePreferredColorScheme();
-
-  useEffect(() => {
-    logger.debug('ColorSchemeProvider', { colorScheme });
-  }, [colorScheme]);
-
   return (
-    <ColorSchemeContext.Provider value={{ colorScheme }}>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
-    </ColorSchemeContext.Provider>
+    </ThemeProvider>
   );
 };
