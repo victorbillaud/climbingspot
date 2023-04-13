d0307cd0931a9d3bbf5f0afcb73f07fbc356c9ad
victorbillaud <victorbillaud@gmail.com>
2023-04-13 16:07:44 +0200

    sitemap ready for spot

--- src/app/server-sitemap.xml/route.ts
+++ src/app/server-sitemap.xml/route.ts
@@ -8,10 +8,38 @@ export async function GET(request: Request) {
     client: supabase,
   });
 
-  const paths = slugs.map((slug) => ({
+  const spotsPaths = slugs.map((slug) => ({
     loc: `https://climbingspot.eu${slug}`,
     lastmod: new Date().toISOString(),
   }));
 
-  return getServerSideSitemap(paths);
+  // extract from spotsPaths the paths like /spot/france/paris and /spot/france/lyon
+  const paths: { loc: string; lastmod: string }[] = [];
+
+  spotsPaths.forEach((spotPath) => {
+    const path = spotPath.loc.split('https://climbingspot.eu')[1];
+    const pathArray = path.split('/');
+    const country = pathArray[2];
+    const city = pathArray[3];
+    const countryPath = `/spot/${country}`;
+    const cityPath = `/spot/${country}/${city}`;
+
+    paths.push({
+      loc: `https://climbingspot.eu${countryPath}`,
+      lastmod: new Date().toISOString(),
+    });
+
+    paths.push({
+      loc: `https://climbingspot.eu${cityPath}`,
+      lastmod: new Date().toISOString(),
+    });
+  });
+
+  // remove duplicates
+  const uniquePaths = paths.filter(
+    (thing, index, self) =>
+      index === self.findIndex((t) => t.loc === thing.loc),
+  );
+
+  return getServerSideSitemap([...spotsPaths, ...uniquePaths]);
 }
