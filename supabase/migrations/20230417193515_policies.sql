4c03e682588ed828255eaa916416b60b2c02a0ed
victorbillaud <victorbillaud@gmail.com>
2023-04-17 21:42:25 +0200

    migration

--- supabase/migrations/20230417193515_policies.sql
+++ supabase/migrations/20230417193515_policies.sql
@@ -1,3 +1,4 @@
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
 
 create policy "Enable update for users based on id"
