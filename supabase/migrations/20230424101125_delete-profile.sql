75a76076adec48673e90996a51a86ecc9c3a6cec
victorbillaud <victorbillaud@gmail.com>
2024-01-17 12:15:11 +0100

    clean migrations

--- supabase/migrations/20230424101125_delete-profile.sql
+++
@@ -1,9 +0,0 @@
-alter table "public"."profiles" drop constraint "profiles_id_fkey";
-
-alter table "public"."profiles" alter column "id" drop default;
-
-alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
-
-alter table "public"."profiles" validate constraint "profiles_id_fkey";
-
-
--- supabase/migrations/20231121144629_expo_push_id.sql
+++
@@ -1,3 +0,0 @@
--- Add column expo_push_id to table profiles
-
-ALTER TABLE profiles ADD COLUMN expo_push_id text;
